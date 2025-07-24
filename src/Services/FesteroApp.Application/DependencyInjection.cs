44b1e898cf6c307e3d5277013844e4c5f603d04c
GUILHERME LOPES <guii-lopes@hotmail.com>
2025-07-23 22:03:44 -0300

    feat: sobe projeto

---
+++ src/Services/FesteroApp.Application/DependencyInjection.cs
@@ -0,0 +1,83 @@
+﻿using FesteroApp.Application.UseCases.Companies.CreateCompany;
+using FesteroApp.Application.UseCases.Companies.DeleteCompany;
+using FesteroApp.Application.UseCases.Companies.GetCompany;
+using FesteroApp.Application.UseCases.Companies.UpdateCompany;
+using FesteroApp.Application.UseCases.Users.CreateUser;
+using FesteroApp.Application.UseCases.Users.DeleteUser;
+using FesteroApp.Application.UseCases.Users.GetUser;
+using FesteroApp.Application.UseCases.Users.LoginUser;
+using FesteroApp.Application.UseCases.Users.UpdateUser;
+using Microsoft.Extensions.DependencyInjection;
+using SrShut.Cqrs.Bus;
+using SrShut.Cqrs.Commands;
+using SrShut.Cqrs.Requests;
+
+namespace FesteroApp.Application;
+
+public static class DependencyInjection
+{
+    public static void AddApplication(this IServiceCollection services)
+    {
+        RegisterCommands(services);
+        RegisterQueries(services);
+    }
+
+    private static void RegisterCommands(this IServiceCollection services)
+    {
+        services.AddSingleton<CreateUserHandler>();
+        services.AddSingleton<UpdateUserHandler>();
+        services.AddSingleton<DeleteUserHandler>();
+        services.AddSingleton<LoginUserHandler>();
+        
+        services.AddSingleton<CreateCompanyHandler>();
+        services.AddSingleton<UpdateCompanyHandler>();
+        services.AddSingleton<DeleteCompanyHandler>();
+
+        services.AddSingleton<ICommandBus>(a =>
+        {
+            var commandBus = a.GetService<MemoryContainerBus>() ?? throw new Exception("commandBus is not found");
+
+            var createUserHandler = a.GetService<CreateUserHandler>();
+            commandBus.Register<CreateUserCommand, CreateUserCommandValidator>(createUserHandler!);
+
+            var updateUserHandler = a.GetService<UpdateUserHandler>();
+            commandBus.Register<UpdateUserCommand, UpdateUserCommandValidator>(updateUserHandler!);
+
+            var deleteUserHandler = a.GetService<DeleteUserHandler>();
+            commandBus.Register<DeleteUserCommand, DeleteUserCommandValidator>(deleteUserHandler!);
+            
+            var loginUserHandler = a.GetService<LoginUserHandler>();
+            commandBus.Register<LoginUserCommand, LoginUserCommandValidator>(loginUserHandler!);
+
+            var createCompanyHandler = a.GetService<CreateCompanyHandler>();
+            commandBus.Register<CreateCompanyCommand, CreateCompanyCommandValidator>(createCompanyHandler!);
+
+            var updateCompanyHandler = a.GetService<UpdateCompanyHandler>();
+            commandBus.Register<UpdateCompanyCommand, UpdateCompanyCommandValidator>(updateCompanyHandler!);
+
+            var deleteCompanyHandler = a.GetService<DeleteCompanyHandler>();
+            commandBus.Register<DeleteCompanyCommand, DeleteCompanyCommandValidator>(deleteCompanyHandler!);
+            
+            return commandBus;
+        });
+    }
+
+    private static void RegisterQueries(this IServiceCollection services)
+    {
+        services.AddSingleton<GetCompanyQueryHandler>();
+        services.AddSingleton<GetUserQueryHandler>();
+
+        services.AddSingleton<IRequestBus>(a =>
+        {
+            var queryBus = a.GetService<MemoryContainerBus>() ?? throw new Exception("queryBus is not found");
+
+            var getCompanyQueryHandler = a.GetService<GetCompanyQueryHandler>();
+            queryBus.Register<GetCompanyQuery, GetCompanyQueryResult>(getCompanyQueryHandler!);
+
+            var getUserQueryHandler = a.GetService<GetUserQueryHandler>();
+            queryBus.Register<GetUserQuery, GetUserQueryResult>(getUserQueryHandler!);
+            
+            return queryBus;
+        });
+    }
+}
---
+++ src/Services/FesteroApp.Domain/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using FesteroApp.Domain.Securities;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FesteroApp.Domain;
+
+public static class DependencyInjection
+{
+    public static void AddDomain(this IServiceCollection services)
+    {
+        services.AddSingleton<ITokenGenerator, JwtTokenGenerator>();
+    }
+}
